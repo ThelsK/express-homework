ca04ece754bbecd7260d9624bc3cdb69b8500e7d
ThelsK <github@thels.nl>
2019-09-06 22:08:35 +0200

    Add example movies

--- sequelize-rest.js
+++ sequelize-rest.js
@@ -6,9 +6,29 @@ const database = new Sequelize(databaseUrl)
 database
   .sync()
   .then(() => console.log("Database schema updated"))
+  .then(() => Promise.all(examples.map(example => Movie.create(example))))
   .catch(console.error)
 
 module.exports = database
+
+const Movie = require("./movie/model")
+const examples = [
+  {
+    title: "Alien",
+    yearOfRelease: 1979,
+    synopsis: "After a space merchant vessel perceives an unknown transmission as a distress call, its landing on the source moon finds one of the crew attacked by a mysterious lifeform, and they soon realize that its life cycle has merely begun.",
+  },
+  {
+    title: "Predator",
+    yearOfRelease: 1987,
+    synopsis: "A team of commandos on a mission in a Central American jungle find themselves hunted by an extraterrestrial warrior.",
+  },
+  {
+    title: "Terminator",
+    yearOfRelease: 1984,
+    synopsis: "A seemingly indestructible robot is sent from 2029 to 1984 to assassinate a young waitress, whose unborn son will lead humanity in a war against sentient machines, while a human soldier from the same war is sent to protect her at all costs.",
+  },
+]
 
 
 
